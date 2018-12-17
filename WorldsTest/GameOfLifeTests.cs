a87465326e4653182c51c9b4acf423290fbb8cbb
Jörg Wunderlich-Pfeiffer <joerg.wunderlich@gmail.com>
2018-12-17 21:57:52 +0100

    added a simple test, more to come after the code is testable

---
+++ WorldsTest/GameOfLifeTests.cs
@@ -0,0 +1,43 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ *  Worlds - program to visualize cellular automata                          *
+ *  Copyright (C) 2018  Jörg Wunderlich-Pfeiffer                             *
+ *                                                                           *
+ *  This program is free software: you can redistribute it and/or modify     *
+ *  it under the terms of the GNU General Public License as published by     *
+ *  the Free Software Foundation, either version 3 of the License, or        *
+ *  (at your option) any later version.                                      *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *  GNU General Public License for more details.                             *
+ *                                                                           *
+ *  You should have received a copy of the GNU General Public License        *
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.   *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+using DataView;
+using System.Drawing;
+using Xunit;
+
+namespace WorldsTest
+{
+    public class GameOfLifeTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            int x = 10;
+            int y = 10;
+
+            IWorld w = new GameOfLife(x,y);
+            int i = 0;
+            foreach (Point p in w.Squares)
+            {
+                i++;
+            }
+            Assert.True(i>=0);
+            Assert.True(i <= x * y);
+        }
+    }
+}
